5d98e110d8d050e38855d8a94971430e49ca6fac
harleynghx <harleynghx.edu@gmail.com>
2026-05-19 15:30:28 +0800

    ex00

--- ex00/srcs/Bureaucrat.hpp
+++ ex00/srcs/Bureaucrat.hpp
@@ -11,17 +11,23 @@ private:
    int _grade;
 public:
     Bureaucrat(std::string const name, int grade);
+    // OCF
     Bureaucrat();
     Bureaucrat(const Bureaucrat& other);
     Bureaucrat& operator=(const Bureaucrat& other);
     ~Bureaucrat();
 
+    // getters
     std::string getName() const;
     int getGrade() const;
 
+    // ++ && --
     void incrementGrade() ;
     void decrementGrade() ;
 
+
+    // custom exception class ( inherit from std::exception )
+    // public inheritance =  "is a" relationship ( else compiler won't know ur custom exception is part of std::exception )
     class GradeTooHighException : public std::exception {
     public:
         virtual const char* what() const throw();
@@ -32,6 +38,6 @@ public:
         virtual const char* what() const throw();
     };
 };
-
+// return ostream& to allow chaining ( cout << b1 << b2 << endl )
 std::ostream &operator<<(std::ostream &os, Bureaucrat const &b);
 #endif
