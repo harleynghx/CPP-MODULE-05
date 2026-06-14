d06f2c5bd3c619b8e2d711f4db781c630b0f0af5
harleynghx <harleynghx.edu@gmail.com>
2026-06-14 12:17:21 +0800

    Aform

--- ex03/srcs/AForm.hpp
+++ ex03/srcs/AForm.hpp
@@ -23,9 +23,34 @@ public:
 
     AForm(const std::string name, int gradeToSign, int gradeToExecute);
 
-    
+    std::string getName() const;
+    bool getIsSigned() const;
+    int getGradeToSigned() const;
+    int getGradeToExecute() const;
 
+    void beSigned(const Bureaucrat &b);
+
+    //= 0 makes it a true virtual
+    virtual void execute(const Bureaucrat &executor) const = 0;
+
+    void checkRequirements(const Bureaucrat &executor) const;
+
+    class GradeTooHighException : public std::exception {
+    public:
+        const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+    public:
+        const char* what() const throw();
+    };
+
+    class NotSignedException : public std::exception {
+    public:
+        const char* what() const throw();
+    };
 };
 
+std::ostream& operator<<(std::ostream& os, const AForm f);
 
 #endif AFORM_HPP
